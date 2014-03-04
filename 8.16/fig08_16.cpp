31f55393ef495cbd92ded62113d0f1051c2b7b7f
227442698 <227442698@qq.com>
2014-03-04 20:59:14 +0800

    add 8.7 sizeof

---
+++ 8.16/fig08_16.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+
+size_t getSize( double * );
+
+int main()
+{
+	double array[20];
+
+	cout << "The number of bytes in the array is " << sizeof( array );
+
+	cout << "\nThe number of bytes returned by getSize is "
+		<< getSize( array ) << endl;
+	return 0;
+}
+
+size_t getSize( double *ptr )
+{
+	return sizeof( ptr );
+}
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ source code				section				description
 * 7.10/fig07_10.cpp		7.4
 * 7.11/fig07_11.cpp		7.4
 * 7.14/fig07_14.cpp     7.5
+* 8.7/fig08_16.cpp		8.7  sizeof‘ÀÀ„∑˚
